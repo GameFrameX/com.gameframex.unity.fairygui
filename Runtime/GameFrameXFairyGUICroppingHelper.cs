dfad85286cf90dfc6d7b7414190a4eee34ae6e26
AlianBlank <wangfj11@foxmail.com>
2024-04-10 18:03:18 +0800

    [增加]1. 增加防裁剪标记

--- Runtime/GameFrameXFairyGUICroppingHelper.cs
+++ Runtime/GameFrameXFairyGUICroppingHelper.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace GameFrameX.FairyGUI.Runtime
 {
+    [Preserve]
     public class GameFrameXFairyGUICroppingHelper : MonoBehaviour
     {
+        [Preserve]
         private void Start()
         {
+            _ = typeof(CloseUIFormCompleteEventArgs);
+            _ = typeof(IUIForm);
+            _ = typeof(IUIFormHelper);
+            _ = typeof(IUIGroup);
+            _ = typeof(IUIGroupHelper);
+            _ = typeof(IUIManager);
+            _ = typeof(OpenUIFormDependencyAssetEventArgs);
+            _ = typeof(OpenUIFormFailureEventArgs);
+            _ = typeof(OpenUIFormSuccessEventArgs);
+            _ = typeof(OpenUIFormUpdateEventArgs);
+            _ = typeof(UIManager);
+            _ = typeof(UILayer);
+            _ = typeof(GObjectHelper);
+            _ = typeof(FUI);
+            _ = typeof(FairyGUIComponent);
+            _ = typeof(FairyGUILoadAsyncResourceHelper);
+            _ = typeof(FairyGUIPackageComponent);
+            _ = typeof(FairyGUIPathFinderHelper);
         }
     }
 }
