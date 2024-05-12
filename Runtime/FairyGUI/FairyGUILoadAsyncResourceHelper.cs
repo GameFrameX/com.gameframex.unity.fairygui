b8565e3c60664b70d55c16725385217b37c9b5ee
AlianBlank <wangfj11@foxmail.com>
2024-05-12 18:28:56 +0800

    [增加]1. 增加不同资源类型的加载方式

--- Runtime/FairyGUI/FairyGUILoadAsyncResourceHelper.cs
+++ Runtime/FairyGUI/FairyGUILoadAsyncResourceHelper.cs
@@ -8,12 +8,44 @@ namespace GameFrameX.FairyGUI.Runtime
 {
     internal class FairyGUILoadAsyncResourceHelper : IAsyncResource
     {
-        public async void LoadResource(string assetName, Action<bool, object> action)
+        public async void LoadResource(string assetName, string extension, Action<bool, object> action)
         {
             var assetComponent = GameEntry.GetComponent<AssetComponent>();
-            var textAsset = await assetComponent.LoadAssetAsync<TextAsset>(assetName);
-            Log.Info(assetName);
-            action.Invoke(textAsset != null && textAsset.AssetObject != null, textAsset?.GetAssetObject<TextAsset>());
+            switch (extension)
+            {
+                case Utility.Const.FileNameSuffix.Binary:
+                {
+                    var assetHandle = await assetComponent.LoadAssetAsync<TextAsset>(assetName);
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetHandle?.GetAssetObject<TextAsset>());
+                    break;
+                }
+                case Utility.Const.FileNameSuffix.PNG: //如果FGUI导出时没有选择分离通明通道，会因为加载不到!a结尾的Asset而报错，但是不影响运行
+                {
+                    if (assetName.IndexOf("!a", StringComparison.OrdinalIgnoreCase) > -1)
+                    {
+                        action.Invoke(false, null);
+                        break;
+                    }
+
+                    var assetHandle = await assetComponent.LoadAssetAsync<Texture>(assetName);
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetHandle?.GetAssetObject<Texture>());
+                    break;
+                }
+                case Utility.Const.FileNameSuffix.Wav:
+                case Utility.Const.FileNameSuffix.Mp3:
+                {
+                    var assetHandle = await assetComponent.LoadAssetAsync<AudioClip>(assetName);
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetHandle?.GetAssetObject<AudioClip>());
+                    break;
+                }
+                default:
+                {
+                    // assetComponent.LoadAssetAsync(assetName,)
+                    // var req = _assetComponent.LoadAssetSync(uiNamePath, type);
+                    // return req.AssetObject;
+                    break;
+                }
+            }
         }
 
         public void ReleaseResource(object obj)
