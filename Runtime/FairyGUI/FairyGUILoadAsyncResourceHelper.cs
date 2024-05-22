d49210a48a38f17fed4eac6864dbb2179414903a
Blank <wangfj11@foxmail.com>
2024-05-23 00:51:17 +0800

    [修改]1. 修改资源的加载路径的回调处理

--- Runtime/FairyGUI/FairyGUILoadAsyncResourceHelper.cs
+++ Runtime/FairyGUI/FairyGUILoadAsyncResourceHelper.cs
@@ -8,7 +8,7 @@ namespace GameFrameX.FairyGUI.Runtime
 {
     internal class FairyGUILoadAsyncResourceHelper : IAsyncResource
     {
-        public async void LoadResource(string assetName, string extension, Action<bool, object> action)
+        public async void LoadResource(string assetName, string extension, Action<bool, string, object> action)
         {
             var assetComponent = GameEntry.GetComponent<AssetComponent>();
             switch (extension)
@@ -16,30 +16,32 @@ namespace GameFrameX.FairyGUI.Runtime
                 case Utility.Const.FileNameSuffix.Binary:
                 {
                     var assetHandle = await assetComponent.LoadAssetAsync<TextAsset>(assetName);
-                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetHandle?.GetAssetObject<TextAsset>());
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<TextAsset>());
                     break;
                 }
                 case Utility.Const.FileNameSuffix.PNG: //如果FGUI导出时没有选择分离通明通道，会因为加载不到!a结尾的Asset而报错，但是不影响运行
                 {
                     if (assetName.IndexOf("!a", StringComparison.OrdinalIgnoreCase) > -1)
                     {
-                        action.Invoke(false, null);
+                        action.Invoke(false, assetName, null);
                         break;
                     }
 
                     var assetHandle = await assetComponent.LoadAssetAsync<Texture>(assetName);
-                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetHandle?.GetAssetObject<Texture>());
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<Texture>());
                     break;
                 }
                 case Utility.Const.FileNameSuffix.Wav:
                 case Utility.Const.FileNameSuffix.Mp3:
                 {
                     var assetHandle = await assetComponent.LoadAssetAsync<AudioClip>(assetName);
-                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetHandle?.GetAssetObject<AudioClip>());
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<AudioClip>());
                     break;
                 }
                 default:
                 {
+                    Log.Error("加载资源失败 Unknown file type: " + assetName + " extension: " + extension);
+                    action.Invoke(false, assetName, null);
                     // assetComponent.LoadAssetAsync(assetName,)
                     // var req = _assetComponent.LoadAssetSync(uiNamePath, type);
                     // return req.AssetObject;
