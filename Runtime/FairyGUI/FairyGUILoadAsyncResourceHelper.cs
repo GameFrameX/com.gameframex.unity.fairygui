562a1c1e2f3bc4fbd5a2cb33ae450e9f14eec661
Blank <wangfj11@foxmail.com>
2024-06-24 22:35:48 +0800

    [增加]1. 增加Spine 的支持

--- Runtime/FairyGUI/FairyGUILoadAsyncResourceHelper.cs
+++ Runtime/FairyGUI/FairyGUILoadAsyncResourceHelper.cs
@@ -8,18 +8,20 @@ namespace GameFrameX.FairyGUI.Runtime
 {
     internal class FairyGUILoadAsyncResourceHelper : IAsyncResource
     {
-        public async void LoadResource(string assetName, string extension, Action<bool, string, object> action)
+        public async void LoadResource(string assetName, string extension, PackageItemType type, Action<bool, string, object> action)
         {
             var assetComponent = GameEntry.GetComponent<AssetComponent>();
-            switch (extension)
+            switch (type)
             {
-                case Utility.Const.FileNameSuffix.Binary:
+                case PackageItemType.Spine:
+                case PackageItemType.Misc:
                 {
                     var assetHandle = await assetComponent.LoadAssetAsync<TextAsset>(assetName);
                     action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<TextAsset>());
                     break;
                 }
-                case Utility.Const.FileNameSuffix.PNG: //如果FGUI导出时没有选择分离通明通道，会因为加载不到!a结尾的Asset而报错，但是不影响运行
+                case PackageItemType.Atlas:
+                case PackageItemType.Image: //如果FGUI导出时没有选择分离通明通道，会因为加载不到!a结尾的Asset而报错，但是不影响运行
                 {
                     if (assetName.IndexOf("!a", StringComparison.OrdinalIgnoreCase) > -1)
                     {
@@ -31,17 +33,46 @@ namespace GameFrameX.FairyGUI.Runtime
                     action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<Texture>());
                     break;
                 }
-                case Utility.Const.FileNameSuffix.Wav:
-                case Utility.Const.FileNameSuffix.Mp3:
+                case PackageItemType.Sound:
                 {
                     var assetHandle = await assetComponent.LoadAssetAsync<AudioClip>(assetName);
                     action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<AudioClip>());
                     break;
                 }
+                case PackageItemType.Font:
+                {
+                    var assetHandle = await assetComponent.LoadAssetAsync<Font>(assetName);
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<Font>());
+                }
+                    break;
+//                 case PackageItemType.Spine:
+//                 {
+// #if FAIRYGUI_SPINE
+//                     var assetHandle = await assetComponent.LoadAssetAsync<Spine.Unity.SkeletonDataAsset>(assetName);
+//                     action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<Spine.Unity.SkeletonDataAsset>());
+// #else
+//                             Log.Error("加载资源失败.暂未适配 Unknown file type: " + assetName + " extension: " + extension);
+//                             action.Invoke(false, assetName, null);
+// #endif
+//                 }
+                    // break;
+                case PackageItemType.DragoneBones:
+                {
+#if FAIRYGUI_DRAGONBONES
+                    var assetHandle = await assetComponent.LoadAssetAsync<DragonBones.DragonBonesData>(assetName);
+                    action.Invoke(assetHandle != null && assetHandle.AssetObject != null, assetName, assetHandle?.GetAssetObject<DragonBones.DragonBonesData>());
+#else
+                    Log.Error("加载资源失败.暂未适配 Unknown file type: " + assetName + " extension: " + extension);
+                    action.Invoke(false, assetName, null);
+#endif
+                }
+                    break;
                 default:
                 {
                     Log.Error("加载资源失败 Unknown file type: " + assetName + " extension: " + extension);
                     action.Invoke(false, assetName, null);
+
+
                     // assetComponent.LoadAssetAsync(assetName,)
                     // var req = _assetComponent.LoadAssetSync(uiNamePath, type);
                     // return req.AssetObject;
