9e985f0a8edbdef76b037f590a4fa9be83c0f9b5
Blank <wangfj11@foxmail.com>
2024-08-24 20:21:25 +0800

    [修改]1. 修改裁剪代码被丢弃的问题

--- Runtime/GameFrameXFairyGUICroppingHelper.cs
+++ Runtime/GameFrameXFairyGUICroppingHelper.cs
@@ -6,27 +6,31 @@ namespace GameFrameX.FairyGUI.Runtime
     [Preserve]
     public class GameFrameXFairyGUICroppingHelper : MonoBehaviour
     {
+        private System.Type[] _types;
         [Preserve]
         private void Start()
         {
-            _ = typeof(CloseUIFormCompleteEventArgs);
-            _ = typeof(IUIForm);
-            _ = typeof(IUIFormHelper);
-            _ = typeof(IUIGroup);
-            _ = typeof(IUIGroupHelper);
-            _ = typeof(IUIManager);
-            _ = typeof(OpenUIFormDependencyAssetEventArgs);
-            _ = typeof(OpenUIFormFailureEventArgs);
-            _ = typeof(OpenUIFormSuccessEventArgs);
-            _ = typeof(OpenUIFormUpdateEventArgs);
-            _ = typeof(UIManager);
-            _ = typeof(UILayer);
-            _ = typeof(GObjectHelper);
-            _ = typeof(FUI);
-            _ = typeof(FairyGUIComponent);
-            _ = typeof(FairyGUILoadAsyncResourceHelper);
-            _ = typeof(FairyGUIPackageComponent);
-            _ = typeof(FairyGUIPathFinderHelper);
+            _types = new[]
+            {
+                typeof(CloseUIFormCompleteEventArgs),
+                typeof(IUIForm),
+                typeof(IUIFormHelper),
+                typeof(IUIGroup),
+                typeof(IUIGroupHelper),
+                typeof(IUIManager),
+                typeof(OpenUIFormDependencyAssetEventArgs),
+                typeof(OpenUIFormFailureEventArgs),
+                typeof(OpenUIFormSuccessEventArgs),
+                typeof(OpenUIFormUpdateEventArgs),
+                typeof(UIManager),
+                typeof(UILayer),
+                typeof(GObjectHelper),
+                typeof(FUI),
+                typeof(FairyGUIComponent),
+                typeof(FairyGUILoadAsyncResourceHelper),
+                typeof(FairyGUIPackageComponent),
+                typeof(FairyGUIPathFinderHelper),
+            };
         }
     }
 }
